a2927c98ac7e9dc0061e1b5ad56a6437200427c3
barsonyj <barsony@kkando.hu>
2025-01-29 21:52:51 +0100

    GET adatok

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let adatok = [
+    { id:1, nev:"porszívó", szin:"piros" },
+    { id:2, nev:"hajszárító", szin:"zöld" },
+    { id:3, nev:"falióra", szin:"fehér" }
+]
+let nextId = 4;
+
 app.get("/", (req, res) => res.send("<h1>Ágak v1.0.0</h1>"))
+app.get("/adatok", (req, res) => res.send(adatok));
 
 app.listen(88, (error) => {
     if (error) console.log(error); else console.log("Server on port 88");
